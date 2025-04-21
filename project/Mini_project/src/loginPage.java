973bbe6270f4c6c697ca6b8bac20ee7c0baa324d
vihangamanusha <vihangamanusha07@gmail.com>
2025-04-22 01:36:36 +0530

    update login gui

--- project/Mini_project/src/loginPage.java
+++ project/Mini_project/src/loginPage.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class loginPage {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton LOGINButton;
+    private JButton RESETButton;
+    private JPanel LoginPage;
 }
