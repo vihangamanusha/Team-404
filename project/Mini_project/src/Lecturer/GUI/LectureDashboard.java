2deea40b277e2e90ea6b8fb3aadb929224eb0c66
Chamod pinimal <chamod_20221397@fot.ruh.ac.lk>
2025-04-22 21:48:56 +0530

    create lecturer dashboard

---
+++ project/Mini_project/src/Lecturer/GUI/LectureDashboard.java
@@ -0,0 +1,90 @@
+package Lecturer.GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LectureDashboard {
+
+
+    private JButton viewDetailes;
+    private JButton attendence;
+    private JButton medicalRecords;
+    private JButton manage;
+    private JButton viewMarks;
+    private JButton calculateMarks;
+    private JButton CAmarks;
+    private JButton FinalMarks;
+    private JButton notification;
+    private JButton editProfile;
+    private JButton logout;
+
+    public LectureDashboard() {
+        viewDetailes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        attendence.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        medicalRecords.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        manage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewMarks.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        calculateMarks.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        CAmarks.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        FinalMarks.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        notification.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        editProfile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        logout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
