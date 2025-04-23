93fc1b2e3e80e26ef8117a62d9f6bcda55768b1d
Gave-shi <159685938+Gave-shi@users.noreply.github.com>
2025-04-23 14:28:28 +0530

    Student GUI
    craeted student GUI with relavnt function buttns

---
+++ project/Mini_project/src/studentPage.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class studentPage {
+    private JButton LOGOUTButton;
+    private JButton EDITUSER;
+    private JButton viewAttendance;
+    private JButton viewMedical;
+    private JButton viewCourseDetails;
+    private JButton viewGrade;
+    private JButton viewTimeTable;
+    private JButton viewNotice;
+}
