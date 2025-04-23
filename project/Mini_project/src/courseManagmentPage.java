b34bf38dafeb86525b3edbebff750c8e0a7777e1
vihangamanusha <vihangamanusha07@gmail.com>
2025-04-23 07:58:17 +0530

    coursemanagement page reset button

--- project/Mini_project/src/courseManagmentPage.java
+++ project/Mini_project/src/courseManagmentPage.java
@@ -1,15 +1,29 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class courseManagmentPage {
     private JButton button1;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
-    private JTextField textField1;
+    private JComboBox courseid;
+    private JComboBox status;
+    private JComboBox lecturerid;
+    private JTextField material;
     private JButton MATERIALSButton;
     private JButton INSERTButton;
     private JButton UPDATEButton;
     private JButton REMOVEButton;
     private JButton RESETButton;
-    private JTextField textField2;
+    private JTextField search;
+
+    public courseManagmentPage() {
+        RESETButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                courseid.setSelectedIndex(0);
+                status.setSelectedIndex(0);
+                lecturerid.setSelectedIndex(0);
+                
+            }
+        });
+    }
 }
