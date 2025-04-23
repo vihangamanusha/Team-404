68a120017525739d914f805a1d4d92f97c63007b
vihangamanusha <vihangamanusha07@gmail.com>
2025-04-23 07:51:52 +0530

    create  course managament page

--- project/Mini_project/src/courseManagmentPage.java
+++ project/Mini_project/src/courseManagmentPage.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class courseManagmentPage {
+    private JButton button1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextField textField1;
+    private JButton MATERIALSButton;
+    private JButton INSERTButton;
+    private JButton UPDATEButton;
+    private JButton REMOVEButton;
+    private JButton RESETButton;
+    private JTextField textField2;
 }
