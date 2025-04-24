3cf40584fe54d13894b09e07331933de28fcfb4f
Gave-shi <159685938+Gave-shi@users.noreply.github.com>
2025-04-25 02:07:00 +0530

    grdae
    Create the grdae GUI

--- project/Mini_project/src/ViewGradeDetails.java
+++ project/Mini_project/src/ViewGradeDetails.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class ViewGradeDetails {
+    private JPanel Dashboard;
+    private JTextField SID;
+    private JTextField CourseId;
+    private JButton viewGRADEButton;
+    private JTable table1;
 }
