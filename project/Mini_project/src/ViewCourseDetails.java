ad944fe17b0c2481bd788657d5058e81ff3d17ba
Gave-shi <159685938+Gave-shi@users.noreply.github.com>
2025-04-24 22:45:34 +0530

    create course
    course details GUI

--- project/Mini_project/src/ViewCourseDetails.java
+++ project/Mini_project/src/ViewCourseDetails.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class ViewCourseDetails {
+    private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton viewCourseDetailsButton;
+    private JTextArea textArea1;
 }
