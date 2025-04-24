29a103880d80feae7bd41bdd2ebb128a0d4bf80f
Gave-shi <159685938+Gave-shi@users.noreply.github.com>
2025-04-25 01:37:32 +0530

    edit GUI
    edit student GUI

--- project/Mini_project/src/studentPage.java
+++ project/Mini_project/src/studentPage.java
@@ -37,6 +37,31 @@ public class studentPage {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
         frame.setVisible(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
