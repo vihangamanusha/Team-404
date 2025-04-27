9b48353654accccca375c8b6e1da8c3b7413b70e
vihangamanusha <vihangamanusha07@gmail.com>
2025-04-28 00:00:40 +0530

    update final

--- project/Mini_project/src/DBConnection.java
+++ project/Mini_project/src/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
     public class DBConnection {
         private static final String URL = "jdbc:mysql://localhost:3306/Tecmis";
         private static final String USER = "root";
-        private static final String PASSWORD = "1234";
+        private static final String PASSWORD = "801@Vihanga";
 
         public static Connection getConnection() throws SQLException {
             return DriverManager.getConnection(URL, USER, PASSWORD);
